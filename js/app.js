f8df63b9400fb9b9082d961c57a5d4e59ac4766f
Leigh Prince <leighpri@gmail.com>
2015-08-11 19:10:27 -0400

    javascript working actually correctly and blog-slash-tumblr link added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,26 @@
 //Laptop version will be added and commented out so I can work on this back and forth during Code Louisville
 //Will replace with live site code when site is uploaded
 
+//get url
+var fullUrl = window.location.href;
+console.log(fullUrl);
+var htmlIndex; //holds index location of whatever.html beginning
+var shortUrl; //holds shortened url
+
+//go from 0 to found index to place that bit of the URL into a new string
+//append that into the below code to replace the file:/// lines
+
+//gets index place for pageID.html and searches for the index for index.html if not found
+htmlIndex = fullUrl.indexOf(pageID);
+  if (htmlIndex == -1) {
+    htmlIndex = fullUrl.indexOf("index");
+  }
+  console.log(htmlIndex);
+
+
+shortUrl = fullUrl.slice(0,htmlIndex);
+console.log(shortUrl);
+
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37: //left
@@ -14,7 +34,7 @@ document.onkeydown = function(e) {
             } else {
             //If not, -- pageID int variable and go to that location.html
               pageID--;
-              window.location = "file:///C:/Users/Leigh's PC/Documents/Coding/Oculoids_Site/" + pageID + ".html";
+              window.location = shortUrl + pageID + ".html";
             }
             // This is the syntax for moving pages: window.location = "http://www.google.com";
             break;
@@ -26,7 +46,7 @@ document.onkeydown = function(e) {
           } else {
           //If not, ++ pageID int variable and go to that location.html
             pageID++;
-            window.location = "file:///C:/Users/Leigh's PC/Documents/Coding/Oculoids_Site/" + pageID + ".html";
+            window.location = shortUrl + pageID + ".html";
           }
             break;
     }
