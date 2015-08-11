207d6008f3a3eb0e033013efe839b13d5bb1d600
Leigh Prince <leighpri@gmail.com>
2015-08-11 19:53:21 -0400

    started media queries

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
-//Eventually I want to add the ability for the code to check for the lead-in URL
-//and parse the string into getting a full new URL
-//The C drive URL for now only works on my desktop
-//Laptop version will be added and commented out so I can work on this back and forth during Code Louisville
-//Will replace with live site code when site is uploaded
-
 //get url
 var fullUrl = window.location.href;
 console.log(fullUrl);
 var htmlIndex; //holds index location of whatever.html beginning
 var shortUrl; //holds shortened url
-
-//go from 0 to found index to place that bit of the URL into a new string
-//append that into the below code to replace the file:/// lines
 
 //gets index place for pageID.html and searches for the index for index.html if not found
 htmlIndex = fullUrl.indexOf(pageID);
@@ -20,7 +11,7 @@ htmlIndex = fullUrl.indexOf(pageID);
   }
   console.log(htmlIndex);
 
-
+//splices url to get final string
 shortUrl = fullUrl.slice(0,htmlIndex);
 console.log(shortUrl);
 
