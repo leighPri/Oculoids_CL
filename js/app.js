bd81282f889339694c8fb217cf2fe2a975d6628b
Leigh <leighpri@gmail.com>
2015-09-08 15:05:50 -0400

    fiddled with css some more

--- js/app.js
+++ js/app.js
@@ -71,3 +71,6 @@ document.onkeydown = function(e) {
             break;
     }
 };
+
+//twitter's widget js info
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
